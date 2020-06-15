7a8889bc845d54c30acd69a8fa1c2dedd62bcec1
surendran610 <66945621+surendran610@users.noreply.github.com>
2020-06-15 15:26:59 +0530

    initial commit

---
+++ fse_projectmanager_ddl.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS project_manager;
+CREATE DATABASE IF NOT EXISTS project_manager;
+USE project_manager;
+
+DROP TABLE IF EXISTS `users`, `user_project`, `task`, `project`, `parent_task`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `emp_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `emp_id_UNIQUE` (`emp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `project` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `project` varchar(45) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `priority` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `project_UNIQUE` (`project`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `parent_task` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_task` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `task` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `task` varchar(45) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `project_id` bigint(20) NOT NULL,
+  `parent_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `priority` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `fk_project_id_idx` (`project_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  KEY `fk_parent_id_idx` (`parent_id`),
+  CONSTRAINT `fkey_parenttask_id` FOREIGN KEY (`parent_id`) REFERENCES `parent_task` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fkey_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fkey_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
